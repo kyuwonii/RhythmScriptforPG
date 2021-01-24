5f73ad95e769feae56e37f0f52cdab2e1d871557
kyuwonii <45291344+kyuwonii@users.noreply.github.com>
2021-01-24 12:42:19 +0900

    Add files via upload

---
+++ Scripts/Home/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Player : MonoBehaviour
+{
+    public int PlayerClass { set; get; }
+
+    public bool isEditMode = false;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
